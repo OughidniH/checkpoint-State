8c030d47f1746adbc8754e032e06932de087209f
HALIMA OUGHIDNI <oughidnih@gmail.com>
2023-04-04 10:31:34 +0100

    scommit

--- src/Comp/Projet.js
+++ src/Comp/Projet.js
@@ -1,11 +1,26 @@
 import { Component } from "react";
 import Image from "./picture.jpg";
-import "./Projet.css"
+import "./Projet.css";
 
 class Projet extends Component {
-  //   constructor(props){
-  //     super(props)
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: 0,
+      interval: null,
+    };
+  }
 
+  componentDidMount() {
+    this.setState({
+      interval: setInterval(() => {
+        this.setState({ timer: this.state.timer + 1 });
+      }, 1000),
+    });
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
   Person = {
     fullname: "Halima Oughidni",
     bio: "bio",
@@ -15,15 +30,15 @@ class Projet extends Component {
   render() {
     return (
       <div className="block">
-         <div>
-            <img src={this.Person.imgSrc} alt=""/>
-         </div>
+        <div>
+          <img src={this.Person.imgSrc} alt="" />
+        </div>
         <div className="card">
           <h3 style={{ color: "red" }}>{this.Person.fullname}</h3>
           <h3 style={{ color: "red" }}>{this.Person.bio}</h3>
           <h3 style={{ color: "red" }}>{this.Person.profession}</h3>
+          <h3 style={{ color: "red" }}>{this.state.timer}</h3>
         </div>
-
       </div>
     );
   }
